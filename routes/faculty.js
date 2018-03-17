4686ab524fbb9e14fc6b86721ee43c22ab1885ca
Rohit <agrawalrohit831@gmail.com>
2018-03-18 01:14:28 +0530

    adsad

---
+++ routes/faculty.js
@@ -0,0 +1,44 @@
+var express = require('express'),
+    router  = express.Router(),
+    mongoose    = require('mongoose'),
+    User    = require('../models/user.js'),
+    nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
+
+router.post('/add', function(req, res){
+    async.waterfall([
+        function(callback){
+            var user = new User(req.body);
+            user.save(function (err, newUser) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                }
+                callback(null, newUser);
+            })
+        },
+        function(user, callback){
+            const mailOptions = {
+                to: user.username, // list of receivers
+                subject: 'Sign Up for technovate', // Subject line
+                html: '<a href="http://localhost:3000/auth/signup/' + user.username + '>www.technovate.com/auth/signup</a>'// plain text body
+            };         
+        }
+    ],
+    function(err, result){
+        if(err){
+            console.log(err);
+            
+        }
+    });
+}); 
+
+
+module.exports = router;
